c3d2dfe5fbf69c8697f94283e88bc04dee34533b
Justin L. Bannister <JustinLBannister@gmail.com>
2025-11-24 11:34:15 -0500

    breadscrumbs

--- gmo-2026.js
+++ gmo-2026.js
@@ -200,46 +200,11 @@
 
 })(jQuery);
 
-/* Base link style */
-.rbccm-link {
-  position: relative;
-  display: inline-flex;
-  align-items: center;
-  gap: 6px;                 /* space between text and arrow */
-  padding-bottom: 4px;     /* space between text and underline */
-  border-bottom: 1.5px solid #0051A5;
-  color: #0051A5;
-  text-decoration: none;
-  font-weight: 500;
-  line-height: 1.3;
-}
-
-/* SVG Arrow */
-.rbccm-link::after {
-  content: url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' width='6' height='9' viewBox='0 0 6 9' fill='none'><path d='M1.64326 8.76752L5.71588 5.09649C5.80591 5.01694 5.87736 4.92229 5.92613 4.81802C5.97489 4.71374 6 4.60189 6 4.48893C6 4.37596 5.97489 4.26411 5.92613 4.15984C5.87736 4.05556 5.80591 3.96092 5.71588 3.88137L1.64326 0.25312C1.55396 0.172915 1.44773 0.109254 1.33068 0.0658105C1.21363 0.0223668 1.08809 0 0.961285 0C0.834485 0 0.708939 0.0223668 0.59189 0.0658105C0.474841 0.109254 0.368606 0.172915 0.279313 0.25312C0.100414 0.41345 0 0.630333 0 0.856402C0 1.08247 0.100414 1.29935 0.279313 1.45968L3.67957 4.48893L0.279313 7.51817C0.101861 7.67756 0.00182152 7.89268 0.000760555 8.11717C3.00407e-05 8.22979 0.0242591 8.34143 0.0720592 8.44569C0.119859 8.54995 0.19029 8.64478 0.279313 8.72473C0.365393 8.8078 0.469097 8.87485 0.584433 8.92201C0.699768 8.96916 0.824449 8.99549 0.951272 8.99947C1.07809 9.00345 1.20455 8.985 1.32332 8.94519C1.44209 8.90537 1.55083 8.84499 1.64326 8.76752Z' fill='%230051A5'/></svg>");
-  width: 6px;
-  height: 9px;
-  display: inline-block;
-}
-
-<a href="#" class="rbccm-link">Learn More</a>
-
-Hero:
-1. period
-2. yellow line <div id="f7c750">
-3. 15px instead of 16
-4. link pending - latest article
-5. Add breadcrumb
-
-Share
-1. Align right on mobile?
-
-Articles Market Views:
-1. Article Icons - Will change as well as add content into cards (currently 5 FPOs)
-2. 7 min read hover effect
-
-Sets Us Apart
-1. Sentence case
-2. Padding/alignment 
-3. Underline link
-4. Arrow acive on slider
+<!-- NEW Breadcrumbs -->
+    <nav class="rbccm-hero__breadcrumbs" aria-label="Breadcrumb">
+      <ol class="rbccm-breadcrumbs">
+        <li><a href="/">Home</a></li>
+        <li><a href="/imagine/">RBC Imagine</a></li>
+        <li aria-current="page">Think further forward</li>
+      </ol>
+    </nav>
